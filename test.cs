9df5aa369edf47ee241c36b46925d349375f5da5
Artem <artemtrushnikov@mail.ru>
2021-10-25 21:43:09 +1000

    Adding one more file and deleting one of them

---
+++ test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
